fdbe5ae58a7e4a1a43399cfcc7ac564c94dac357
Jacqueline Lee <anginblu@gmail.com>
2018-01-05 07:02:14 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,28 @@
 class PigLatinizer
 
   def piglatinize(word)
-    arr = word.split("")
-    first = arr.shift
-    last = arr.pop
-    arr.join + last + first + "ay"
+    str1="aeiou"
+    str2=(/\A[aeiou]/)
+    vowel = str1.scan(/\w/)
+    alpha =('a'..'z').to_a
+    con = (alpha - vowel).join
+    word = str.scan(/\w/)  
+    if  #first rule 
+      str =~ str2
+      str + "ay" 
+    elsif # second rule 
+      str != str2 
+      s = str.slice!(/^./)
+      str + s + "ay"
+    elsif 
+      word[0.1]=~(/\A[con]/)
+      s = str.slice!(/^../)
+      str + s + "ay"
+    else
+      word[0..2]=~(/\A[con]/) 
+      s = str.slice!(/^.../)
+      str + s + "ay" 
+    end
   end
 
   def to_pig_latin(text)
