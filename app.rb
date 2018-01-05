02d8491b99680d34f1ed534dd110ad8c57e7e968
Jacqueline Lee <anginblu@gmail.com>
2018-01-05 06:53:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @user_phrase = PigLatinizer.new(params[:user_phrase])
+    new = PigLatinizer.new
+    new.to_pig_latin(params[:user_phrase])
     erb :piglatinize
   end
 end
