e57f5f94440de0606401ae71c2643e3c21a5e360
Jacqueline Lee <anginblu@gmail.com>
2018-01-05 07:12:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,14 @@
 class PigLatinizer
 
   def piglatinize(str)
-    str = str.downcase
+    str2 = str.downcase
     alpha = ('a'..'z').to_a
     vowels = %w[a e i o u]
     consonants = alpha - vowels
 
-    if vowels.include?(str[0])
+    if vowels.include?(str2[0])
       str + 'way'
-    elsif consonants.include?(str[0]) && consonants.include?(str[1])
+    elsif consonants.include?(str2[0]) && consonants.include?(str2[1])
       str[2..-1] + str[0..1] + 'ay'
     elsif consonants.include?(str[0]) && consonants.include?(str[1]) && consonants.include?(str[2])
       'ay' + str
