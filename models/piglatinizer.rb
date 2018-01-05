7a87501b19948a5945819b99eaad1d30f6f914c3
Jacqueline Lee <anginblu@gmail.com>
2018-01-05 06:51:53 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,11 +5,13 @@ class PigLatinizer
   end
 
   def piglatinize(text)
-    text.reverse + "ay"
+    arr = text.split(" ")
+    arr2 = arr.map {|word| word.reverse + "ay"}
+    arr2.join(" ")
   end
 
   def to_pig_latin(text)
     arr = text.split(" ")
-
+    arr.map {|word| piglatinize(word)}
   end
 end
